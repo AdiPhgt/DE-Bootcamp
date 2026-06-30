3918b53d8e2ff0f0d7865e6b9bf6e74a456ff0e4
Aditi Phogat <aditiphogat3174@gmail.com>
2026-07-01 04:04:12 +0530

    Day 1 - Python Basics

--- 01.py
+++ 01.py
@@ -1,1 +1,14 @@
-print("Hello Aditi!")
+name= input("Enter your name: ")
+age = input("Enter your age: ")
+company = input("Enter your company: ")
+favorite_programming_language = input("Enter your favorite programming language: ")
+
+print("\nHello", name,"!")
+print("You are", age, "years old.")
+print("You work at", company,".")
+print("Your favorite programming language is", favorite_programming_language,".")
+
+print(type(name))
+print(type(age))
+print(type(company))
+print(type(favorite_programming_language))
